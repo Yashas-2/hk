ef1c664dfe76dc1380611b408b675c2af0d5eed0
Yashas HK <yashas2k5@gmail.com>
2025-01-11 13:19:52 +0530

    tic tac toe

--- script.js
+++ script.js
@@ -1,65 +1,97 @@
-let userScore=0;
-let compScore=0;
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
 
-const choices = document.querySelectorAll(".choice");
-const msg= document.querySelector("#msg");
-const userScorePara = document.querySelector("#user-score");
-const compScorePara = document.querySelector("#comp-score");
+let turnO = true;
+let count= 0;
 
+const winPattern = [
+    [0,4,8],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+    [0,1,2]
+];
 
-const genCompChoice = () => {
-    const options = ["rock","paper","scissor"];
-    const randIdx= Math.floor(Math.random() * 3);
-    return options[randIdx];
-
-};
-
-const drawGame = () => {
-  console.log("The game is Draw");
-  msg.innerText="The game is Draw";
-  msg.style.backgroundColor = "Orange";
-};
-
-const showWinner = (userWin) => {
-   if(userWin){
-    userScore++;
-    userScorePara.innerText=userScore;
-    console.log("You Win");
-    msg.innerText= "You Win";
-    msg.style.backgroundColor = "green";
-   }else{
-    compScore++;
-    compScorePara.innerText=compScore;
-    console.log("You Lose");
-    msg.innerText= "You Lose";
-    msg.style.backgroundColor = "red";
-   }
-};
-
-const playGame = (userChoice) => {
-       console.log("user-choice = ", userChoice);
-       const compChoice = genCompChoice();
-       console.log("comp-choice = ", compChoice);
-
-
-if(userChoice === compChoice){
-   drawGame();
-}else {
-  let userWin=true;
-  if(userChoice==="rock"){
-    userWin = compChoice === "paper"?false : true ;
-  }else if(userChoice==="paper"){
-    userWin = compChoice === "scissors"?false : true ;
-  }else{
-    userWin = compChoice === "rock"?false : true ;
-  }
-  showWinner(userWin);
+const resetGame = () => {
+    turnO= true;
+    enableBoxes();
+    msgContainer.classList.add("hide")
 }
-};
 
-choices.forEach((choice) => {
-    choice.addEventListener("click", () => {
-      const userChoice = choice.getAttribute("id"); 
-      playGame(userChoice);
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked");
+        if(turnO) {
+            box.innerText = "O";
+            box.style.color = "purple";
+            turnO= false;
+        }else{
+            box.innerText = "X";
+            box.style.color = "orange";
+            turnO= true;
+        }
+        box.disabled= true;
+        count++;
+        
+        let IsWinner=checkWinner();
+
+        if(count===9 && !IsWinner){
+            showDraw();
+        }
     });
   });
+    
+  const showDraw = () => {
+    msg.innerText = `Congratulations This Match is DRAW!`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+
+  const disableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled=true;
+    }
+};
+
+const enableBoxes = () => {
+    for(let box of boxes) {
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations you are the winer, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};  
+
+
+const checkWinner = () => {
+    for(let pattern of winPattern){ 
+        let pos1v=boxes[pattern[0]].innerText;
+        let pos2v=boxes[pattern[1]].innerText;
+        let pos3v= boxes[pattern[2]].innerText;
+
+        if(pos1v!="" && pos2v!="" && pos3v!=""){
+            if(pos1v===pos2v && pos2v===pos3v){
+                console.log("winner",pos1v);
+                showWinner(pos1v);
+            }
+        }
+
+    } 
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
